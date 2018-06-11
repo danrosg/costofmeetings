46392455e175738baa11d4390c8a42536826cba6
Daniel Rosales Garcia <drosales@starbucks.com>
2018-06-11 12:57:29 -0700

    adding code to deploy to azure

---
+++ deploy/deployAzure.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+
+az webapp deployment user set --user-name costofmeetings --password Pass@@%%202
+az group create --name hackOptimizersRG --location "eastus"
+az appservice plan create --name hackOptimizersASP --resource-group hackOptimizersRG --sku S1 --is-linux
+az webapp create --resource-group hackOptimizersRG --plan hackOptimizersASP --name hackOptimizers --runtime "NODE|8.11" --deployment-local-git
+
+
+#https://costofmeetings@hackoptimizers.scm.azurewebsites.net/hackOptimizers.git
+
+
+git remote add azure https://costofmeetings@hackoptimizers.scm.azurewebsites.net/hackOptimizers.git
+
+git push azure master
