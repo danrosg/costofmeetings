8a6241f11ad6bd95ddb21cdca248b81aca592cb1
Daniel Rosales Garcia <drosales@starbucks.com>
2018-06-11 22:33:39 -0700

    scripts to deploy to azure

--- deploy/deployAzure.sh
+++ deploy/deployAzure.sh
@@ -4,7 +4,7 @@
 az webapp deployment user set --user-name costofmeetings --password Pass@@%%202
 az group create --name hackOptimizersRG --location "eastus"
 az appservice plan create --name hackOptimizersASP --resource-group hackOptimizersRG --sku S1 --is-linux
-az webapp create --resource-group hackOptimizersRG --plan hackOptimizersASP --name hackOptimizers --runtime "NODE|8.11" --deployment-local-git
+az webapp create --resource-group hackOptimizersRG --plan hackOptimizersASP --name hackOptimizers --runtime "NODE|8.11" --deployment-local-git --startup-file "npm run run_azure"
 
 
 #https://costofmeetings@hackoptimizers.scm.azurewebsites.net/hackOptimizers.git
